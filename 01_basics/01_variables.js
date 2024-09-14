53db84c6dd36316116becdd1849c6cf1a41e900c
MD SHAH FAISAL <143159119+ShahFaisal078@users.noreply.github.com>
2024-09-14 06:03:33 +0000

    Variables in JS

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144553;
+let accountEmail = "mdshahfaisal778.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+
+// accountId = 2 // Not allowed bcoz of variable const 
+
+console.table([accountId, accountEmail, accountPassword, accountCity]); // This will print in a Tabular form 
